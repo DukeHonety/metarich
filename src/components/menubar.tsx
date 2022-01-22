b46e4e3edb53bf370d17163d93d30f50a9dcd714
John Doe <mitchellcaleb00@gmail.com>
2022-01-22 07:15:17 -0800

    chore: complete `header/menus&links`

--- src/components/menubar.tsx
+++ src/components/menubar.tsx
@@ -2,19 +2,48 @@ import React from "react";
 import routes from "../routes";
 
 const MenuBar = () =>{
-    
-    const style:string = "inline-block";
-    return (
-        <>
-            <ul>
-            {
-                routes.map((route,key)=>{
-                    return <li className={style} key={key}><a href={route.path}>{route.name}</a></li>;
-                })
-            }
-            </ul>
-        </>
-    );
+  const style:string = "inline-block text-white p-20p text-20p";
+  const linkUrls = [
+      {
+          name: "",
+          url: "https://www.instagram.com/metarichnft/",
+          content: "",
+          class: "relative top-5p inline-block bg-no-repeat bg-linkBackPos bg-20pauto rounded-full w-30p h-30p mr-5p ml-5p bg-instagram",
+          img: ""
+      },
+      {
+          name: "",
+          url: "http://www.twitter.com/metarichnft",
+          content: "",
+          class: "relative top-5p inline-block bg-no-repeat bg-linkBackPos bg-20pauto rounded-full w-30p h-30p mr-5p ml-5p bg-twitter",
+      },
+      {
+          name: "",
+          url: "https://discord.gg/cJJd8wNvCk",
+          content: "",
+          class: "relative top-5p inline-block bg-no-repeat bg-linkBackPos bg-20pauto rounded-full w-30p h-30p mr-5p ml-5p bg-discord",
+      }
+  ];
+  return (
+    <>
+      <div className="relative float-right">
+        <ul className="inline-block">
+          {
+            routes.map((route,key)=>{
+                return <li className={style} key={key}><a href={route.path}>{route.name}</a></li>;
+            })
+          }
+        </ul>
+        <div className={style} key="work">
+          {
+              linkUrls.map( (link,key) => {
+                  return <a href={link.url} target="_blank" key={key} className={link.class}>{link.content}</a>;
+              })
+          }
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MenuBar;
