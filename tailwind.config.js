07624dc7886121c47a9dea1b7ee7538d925cd6bb
John Doe <mitchellcaleb00@gmail.com>
2022-01-22 05:38:12 -0800

    feat: Add `font-size` and `spacing`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+function range(start, end, increment = 1) {
+  const count = Math.floor((end - start + increment) / increment);
+  return Array(count).fill(0).map((_, idx) => start + idx * increment);
+}
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
     extend: {},
+    fontSize:{
+      ...range(5, 140).reduce((merged, f) => ({ ...merged, [`${f}p`]: `${f}px`}), {}),
+    },
+    spacing:{
+      ...range(5, 140).reduce((merged, f) => ({ ...merged, [`${f}p`]: `${f}px`}), {}),
+    },
   },
   plugins: [],
 }
