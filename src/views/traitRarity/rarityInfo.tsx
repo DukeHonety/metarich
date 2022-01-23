966695a15192a0b94211477272d5e84a19d2275f
John Doe <mitchellcaleb00@gmail.com>
2022-01-23 12:25:19 -0800

    feat: Insert data to `rarityInfo`

--- src/views/traitRarity/rarityInfo.tsx
+++ src/views/traitRarity/rarityInfo.tsx
@@ -48,14 +48,305 @@ const RarityData:any = {
             rate: "2%"
         },
     ],
-    Background: [],
-    Clothes: [],
-    Eyes: [],
-    Hat: [],
-    Necklace: [],
-    Ear: [],
-    Mouth: [],
-    Backpack: [],
+    Background: [{
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18d074048f31dc1615b19_none.png",
+            title: "NONE",
+            rate: "20%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61bef6f7981c8f55551c1eee_3.png",
+            title: "GREY",
+            rate: "13%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17613c8050850d58ed0c3_12.png",
+            title: "BLUE",
+            rate: "11%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61bef6f593b7b0fe8a504ce4_4.png",
+            title: "BLACK",
+            rate: "9%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61bef6f0fa16264bc2ce3a6a_7.png",
+            title: "RED",
+            rate: "8%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18d074048f31dc1615b19_none.png",
+            title: "NONE",
+            rate: "20%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61bef6f7981c8f55551c1eee_3.png",
+            title: "GREY",
+            rate: "13%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17613c8050850d58ed0c3_12.png",
+            title: "BLUE",
+            rate: "11%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61bef6f593b7b0fe8a504ce4_4.png",
+            title: "BLACK",
+            rate: "9%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61bef6f0fa16264bc2ce3a6a_7.png",
+            title: "RED",
+            rate: "8%"
+        }
+    ],
+    Clothes: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17b9cd198bd952df93f99_21.png",
+            title: "SHIRT W",
+            rate: "6%"
+        }
+    ],
+    Eyes: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fbecfa7d7734f28c09b_8.png",
+            title: "CLASSIC",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fbecfa7d7734f28c09b_8.png",
+            title: "CLASSIC",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fbecfa7d7734f28c09b_8.png",
+            title: "CLASSIC",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fbecfa7d7734f28c09b_8.png",
+            title: "CLASSIC",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fbecfa7d7734f28c09b_8.png",
+            title: "CLASSIC",
+            rate: "12%"
+        },
+    ],
+    Hat: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c195625189173e1421ef3e_3.png",
+            title: "WHITE HAT",
+            rate: "10%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c195625189173e1421ef3e_3.png",
+            title: "WHITE HAT",
+            rate: "10%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c195625189173e1421ef3e_3.png",
+            title: "WHITE HAT",
+            rate: "10%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c195625189173e1421ef3e_3.png",
+            title: "WHITE HAT",
+            rate: "10%"
+        },
+    ],
+    Necklace: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },{
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },{
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+    ],
+    Ear: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+    ],
+    Mouth: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+    ],
+    Backpack: [
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c18fa91774e2863e259b3a_15.png",
+            title: "NONE",
+            rate: "12%"
+        },
+    ],
 };
 
 export const GetTypeData = (type: string) =>{
