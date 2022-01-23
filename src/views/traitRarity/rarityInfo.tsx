0a5dd3ec15087c34d11ad3b413e1be1a39f2507e
John Doe <mitchellcaleb00@gmail.com>
2022-01-23 07:11:33 -0800

    feat: Create `traitrairty database`

---
+++ src/views/traitRarity/rarityInfo.tsx
@@ -0,0 +1,62 @@
+
+
+const RarityTypes = () =>{
+    return (
+        <></>
+    );
+}
+
+const TypeRairity = ["Fur", "Background", "Clothes", "Eyes", "Hat", "Necklace", "Ear", "Mouth", "Backpack"];
+export const GetTypes = () =>{
+    return TypeRairity;
+};
+
+const RairityData = {
+    'Fur': [{
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c1787bdf617d02b8f9733c_2.png",
+            title: "CLASSIC",
+            rate: "40%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17881628de522867089ca_7.png",
+            title: "SILVER",
+            rate: "15%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c1787bd1a68a5cbd6fcc63_6.png",
+            title: "BLACK",
+            rate: "15%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c17885e48c2e5fe14e2520_5.png",
+            title: "RADIO",
+            rate: "10%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c1788cb1765f48a82eb5da_1.png",
+            title: "CHEETAH",
+            rate: "10%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c178870870e938850bf1ce_4.png",
+            title: "GOLD",
+            rate: "8%"
+        },
+        {
+            image: "https://uploads-ssl.webflow.com/6190a1f04e2d84fef77d7bdd/61c2351f14f751a9a7e59756_1.png",
+            title: "DIAMOND",
+            rate: "2%"
+        },
+    ],
+    "Background": [],
+    "Clothes": [],
+    "Eyes": [],
+    "Hat": [],
+    "Necklace": [],
+    "Ear": [],
+    "Mouth": [],
+    "Backpack": [],
+};
+
+
+export default RarityTypes;
