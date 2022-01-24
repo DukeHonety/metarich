2da298d330a34c3fbc143488feb8eeed8d1c1c04
John Doe <mitchellcaleb00@gmail.com>
2022-01-23 18:55:22 -0800

    refactor: Move main page from `App.tsx` to `index.tsx`

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Richyshib from './views';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Richyshib></Richyshib>
   </React.StrictMode>,
   document.getElementById('root')
 );
