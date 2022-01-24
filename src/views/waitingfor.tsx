5d14dc66cd82aa4241e5c96283ad7ff2add8c784
John Doe <mitchellcaleb00@gmail.com>
2022-01-23 18:20:15 -0800

    feat: Make `waitForus`

--- src/views/waitingfor.tsx
+++ src/views/waitingfor.tsx
@@ -1,7 +1,9 @@
 const WaitForUs = () =>{
     return (
-        <>
-        </>
+        <div>
+            <h3 className="mt-60p text-Mint text-center text-22p font-semibold mb-10p text-montserrat">What are you waiting for?</h3>
+            <h2 className="text-white mb-20p text-center mt-20p text-36p font-bold text-montserrat">Become a META RICH</h2>
+        </div>
     );
 }
 
