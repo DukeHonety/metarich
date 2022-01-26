339748c50e4505a989d8e10aa3c10d0b2045a6d5
John Doe <mitchellcaleb00@gmail.com>
2022-01-26 09:51:49 -0800

    chore: Remove logo image

--- src/views/warnBeware.tsx
+++ src/views/warnBeware.tsx
@@ -6,7 +6,7 @@ const WarnBeware = () =>{
     return (
         <div className="bg-[#FF0000] p-10p pb-20p my-20p">
             <h1 className="text-center m-20p text-white text-30p text-roboto font-bold">BEWARE OF SCAMMERS</h1>
-            <p className="text-center text-white text-20p text-montserrat font-semibold  ">ONLY MINT ON richyshib.co</p>
+            <p className="text-center text-white text-20p text-montserrat font-semibold  uppercase">ONLY MINT ON richyshib.co</p>
         </div>
     );
 }
